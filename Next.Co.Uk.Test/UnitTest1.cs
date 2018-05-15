e7359f9fc3be003cab6cf555df401c3fcd6febd0
TSConroe <1conroe1@gmail.com>
2018-05-15 20:15:00 +0300

    added selenium chrome driver created first simple test (open google page)

--- Next.Co.Uk.Test/UnitTest1.cs
+++ Next.Co.Uk.Test/UnitTest1.cs
@@ -1,14 +1,43 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 
 namespace Next.Co.Uk.Test
 {
     [TestClass]
     public class Boys1516yJeansTest
     {
+        // Arrange
+
+        IWebDriver driver;
+        string expectTitle = "Google";
+
         [TestMethod]
-        public void TestMethod1()
+        public void BrouserInitialize()
         {
+             driver = new ChromeDriver();
+            
+            driver.Url = "http://google.com";
+
+            //Act
+
+            String actualTitle = driver.Title;
+
+            //Assert
+
+            Assert.AreEqual(expectTitle, actualTitle);
+
+            driver.Close();
         }
+
+
+
+      
+     
+
+        
+
     }
 }
