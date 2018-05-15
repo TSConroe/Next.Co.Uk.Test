113d848a46954b0c7075270a4d1ecd3e78e5c9c3
TSConroe <1conroe1@gmail.com>
2018-05-15 18:01:24 +0300

    Добавьте файлы проекта.

---
+++ Next.Co.Uk.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Next.Co.Uk.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ArtZak")]
+[assembly: AssemblyProduct("Next.Co.Uk.Test")]
+[assembly: AssemblyCopyright("Copyright © ArtZak 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8fb291e6-6ad3-4fdd-a082-0789f1468e19")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
