ef82751a035dc7fc5c5986b495d2746694871dd2
TSConroe <1conroe1@gmail.com>
2018-05-16 14:40:30 +0300

    describe how to find page with necessary products (15-16y old boy jeans)

---
+++ Next.Co.Uk.Test/OpenPageTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace Next.Co.Uk.Test
+{
+    [TestClass]
+    public class OpenPageTest
+    {
+        
+        private IWebDriver driver;
+        string expectTitle = "Buy boys jeans in 1516years from the Next UK online shop";
+        By Jeanstextlink = By.LinkText("Boys");
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+        }
+
+        [TestMethod]
+        public void BrouserInitialize()
+        {
+                         
+            driver.Url = "http://www.next.co.uk/";
+
+            //Act
+
+            String actualTitle = driver.Title;
+
+            //Assert
+
+            Assert.AreEqual(expectTitle, actualTitle);
+                       
+            
+        }
+        [TestMethod]
+        public void SelectCountry()
+        {
+            driver.Url = "http://www.next.co.uk/";
+            By CounteySelectorClose = By.ClassName("flagContainer");
+            IWebElement EnglishButton = driver.FindElement(By.ClassName("languageButton"));
+            EnglishButton.Click();
+            //!!!
+            //Need to add country selector checher
+            //
+            
+
+            //Act
+            driver.FindElement(CounteySelectorClose);
+            IWebElement selectElem = driver.FindElement(By.TagName("select"));
+           
+            String actualTitle = driver.Title;
+
+            //Assert
+
+            Assert.AreEqual(expectTitle, actualTitle);
+
+
+        }
+
+
+        [TestMethod]
+        public void OpenJeansPage()
+        {
+
+            driver.Url = "http://www.next.co.uk/";
+
+            //Find Boys jeans page
+            By Serchbox = By.Id("sli_search_1");
+            driver.FindElement((Serchbox)).SendKeys("BOYS JEANS");
+            driver.FindElement((Serchbox)).Submit();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+
+            //Show sizes list
+            By SizeFiltr = By.XPath("//*[@id='size']/div[1]/p");
+            driver.FindElement(SizeFiltr).Click();
+
+            //Show all sizes
+            By SizeVievMoreLink = By.XPath("//*[@id='size']/a");
+            driver.FindElement(SizeVievMoreLink).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+
+            //Scroll down
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("window.scrollBy(0,500)", "");
+            
+            //chose 15-16y size
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+            By Size1516Y = By.XPath("//*[@id='size']/div[2]/ul/li[21]/div/label");
+            driver.FindElement(Size1516Y).Click();
+
+            //check title
+            String actualTitle = driver.Title;
+
+            Assert.AreEqual(expectTitle, actualTitle);
+
+
+
+        }
+
+
+        [TestCleanup]
+
+        public void BrouserClose()
+        {
+                   
+            
+           // driver.Close();
+
+        }
+        
+
+    }
+}
---
+++ Next.Co.Uk.Test/PageObjects/Homepage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+//using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+
+namespace Next.Co.Uk.Test.PageObjects
+{
+    public class Guru99Login
+    {
+
+        public Guru99Login(IWebDriver driver)
+        {
+
+            // this.driver = driver;
+
+
+
+        }
+    }
+}
