047dafd5290f1a9d5250576aca374274bcf702f8
TSConroe <1conroe1@gmail.com>
2018-05-17 10:48:44 +0300

    price saved (link no), restart and verey is work correct

--- Next.Co.Uk.Test/OpenPageTest.cs
+++ Next.Co.Uk.Test/OpenPageTest.cs
@@ -12,10 +12,9 @@ namespace Next.Co.Uk.Test
     public class OpenPageTest
     {
         
-        private IWebDriver driver;
+        public IWebDriver driver;
         string expectTitle = "Buy boys jeans in 1516years from the Next UK online shop";
-        By Jeanstextlink = By.LinkText("Boys");
-
+        
         [TestInitialize]
         public void SetUp()
         {
@@ -67,13 +66,16 @@ namespace Next.Co.Uk.Test
 
 
         [TestMethod]
-        public void OpenJeansPage()
+        public  void OpenMainJeansPage()
         {
-
+            //
+            //arrange
+            //
             driver.Url = "http://www.next.co.uk/";
 
             //Find Boys jeans page
             By Serchbox = By.Id("sli_search_1");
+            By JeansIMG = By.XPath("//*[@id='fade']");
             driver.FindElement((Serchbox)).SendKeys("BOYS JEANS");
             driver.FindElement((Serchbox)).Submit();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
@@ -99,34 +101,83 @@ namespace Next.Co.Uk.Test
             //check title
             String actualTitle = driver.Title;
 
-            Assert.AreEqual(expectTitle, actualTitle);
+          //  Assert.AreEqual(expectTitle, actualTitle);
 
 
-            ArrayList Titleslist = new ArrayList();
+           ArrayList Titleslist = new ArrayList();
+           ArrayList Linklist = new ArrayList();
             //frst one
-            
-           // object olink = (driver.FindElement(By.CssSelector("#i1>section:nth-child(1)>div:nth-child(1)>h2:nth-child(1)>a*")).GetAttribute("href"));
 
-            //Titleslist.Add(driver.FindElement(By.XPath("//*[@id='i1']/section/div[1]/h2/a/@href")));
+
+
+            // Titleslist.Add(driver.FindElement(By.XPath("//*[@id='i1']/section/div[2]/a/@href[1]")).Text);
             Titleslist.Add(driver.FindElement(By.XPath("//*[@id='i1']/section/div[1]/div[1]/a")).Text);
-
+            Linklist.Add("http://www.next.co.uk/xo51014s3#187325?i=1516years");
             //second one
-           // Titleslist.Add(driver.FindElement(By.XPath("//*[@id='i2']/section/div[1]/h2/a/@href")).Text);
+            // Titleslist.Add(driver.FindElement(By.XPath("//*[@id='i2']/section/div[1]/h2/a/@href")).Text);
             Titleslist.Add(driver.FindElement(By.XPath("//*[@id='i2']/section/div[1]/div[1]/a")).Text);
+            Linklist.Add("http://www.next.co.uk/g76438s1#165797?i=1516years");
 
             //third one
-           // Titleslist.Add(driver.FindElement(By.XPath("//*[@id='i3']/section/div[1]/h2/a/@href")).Text);
+            // Titleslist.Add(driver.FindElement(By.XPath("//*[@id='i3']/section/div[1]/h2/a/@href")).Text);
             Titleslist.Add(driver.FindElement(By.XPath("//*[@id='i3']/section/div[1]/div[1]/a")).Text);
+            Linklist.Add("http://www.next.co.uk/g892290s2#185536?i=1516years");
 
-          //  System.Console.Write(olink);
-            foreach (object o in Titleslist)
+
+            foreach (object o in Linklist)
             {
                 System.Console.Write(o);
             }
-           /// jeans1516y.Add()
 
 
+            driver.Close();
+
+            //
+            //act
+            //
+            driver = new ChromeDriver();
+            string  driverurl = (string)Linklist[0];
+            driver.Url = driverurl;
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+            if (driver.FindElement(JeansIMG).Displayed)
+                driver.FindElement((JeansIMG)).Click();
+            ArrayList RefreshTitleslist = new ArrayList();
+            RefreshTitleslist.Add(driver.FindElement(By.XPath("//*[@id='Style122122']/section[2]/div[1]/div/span")).Text);
+            driver.Close();
+
+
+            driver = new ChromeDriver();
+            driverurl = (string)Linklist[1];
+            driver.Url = driverurl;
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+            if (driver.FindElement(JeansIMG).Displayed)
+             driver.FindElement((JeansIMG)).Click();
+            RefreshTitleslist.Add(driver.FindElement(By.XPath("//*[@id='Style83513']/section[2]/div[1]/div/span")).Text);
+            driver.Close();
+
+
+            driver = new ChromeDriver();
+            driverurl = (string)Linklist[2];
+            driver.Url = driverurl;
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+            if (driver.FindElement(JeansIMG).Displayed)
+                driver.FindElement((JeansIMG)).Click();
+            RefreshTitleslist.Add(driver.FindElement(By.XPath("//*[@id='Style113745']/section[2]/div[1]/div/span")).Text);
+            driver.Close();
+
+
+            CollectionAssert.AreEqual(RefreshTitleslist, Titleslist);
+            foreach (object o in RefreshTitleslist)
+            {
+                System.Console.Write(o);
+            }
         }
+       
+
+
 
 
         [TestCleanup]
