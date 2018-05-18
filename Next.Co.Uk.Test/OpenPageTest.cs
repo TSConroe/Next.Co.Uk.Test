3520391cfe576ccff61f8311a345f65446bb69be
TSConroe <1conroe1@gmail.com>
2018-05-18 11:27:59 +0300

    tried to use Page Object pattern

--- Next.Co.Uk.Test/OpenPageTest.cs
+++ Next.Co.Uk.Test/OpenPageTest.cs
@@ -65,6 +65,7 @@ namespace Next.Co.Uk.Test
 
 
         }
+        
 
 
         [TestMethod]
--- Next.Co.Uk.Test/UnitTest1.cs
+++ Next.Co.Uk.Test/UnitTest1.cs
@@ -2,25 +2,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Next.Co.Uk.Test.PageObjects;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace Next.Co.Uk.Test
 {
     [TestClass]
     public class UnitTest1
     {
-        public IWebDriver driver;
+        ArrayList Linklist = new ArrayList();
+        ArrayList Pricelist = new ArrayList();
+        public IWebDriver Driver { get; set; }
+        public WebDriverWait Wait { get; set; }
+
         [TestInitialize]
-        public void SetUp()
+        public void SetupTest()
         {
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            this.Driver = new ChromeDriver();
+            this.Wait = new WebDriverWait(this.Driver, TimeSpan.FromSeconds(20));
         }
+
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            this.Driver.Quit();
+        }
+
         [TestMethod]
-        public void TestMethod1()
+        public void SearchJeans()
         {
+            MainPage searchJeansPage = new MainPage(this.Driver);
+            searchJeansPage.Navigate();
+            searchJeansPage.Search("BOYS JEANS");
+            searchJeansPage.SearchSubmit();
+
+            //Show sizes list
+            searchJeansPage.ChangeSizeButton();
+
+            //Scroll down
+            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+            js.ExecuteScript("window.scrollBy(0,500)", "");
+
+            //Show all sizes
+            searchJeansPage.SizeVievMoreLinkMenu();
+            
+            searchJeansPage.SizeVievMenu();
+
+            searchJeansPage.PriceSortMenu();
+            searchJeansPage.GetLink(Linklist);
+            searchJeansPage.GetPrice(Pricelist);
+
+
+            foreach (object o in Linklist)
+            {
+                Console.Write(o);
+                Console.WriteLine();
+            }
+
+            foreach (object o in Pricelist)
+            {
+                Console.Write(o);
+                Console.WriteLine();
+            }
+
         }
+
+        [TestMethod]
+        public void JeansPage()
+        {
+            
+        }
+
     }
 }
